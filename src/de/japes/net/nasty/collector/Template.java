6fed467a5eab73d016147fbea3fb46321a4f0ad0
muenz <muenz@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2006-05-31 15:41:12 +0000

    Added GPL notes
    git-svn-id: file:///Users/braun/svn/vermont/trunk/nasty@562 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- src/de/japes/net/nasty/collector/Template.java
+++ src/de/japes/net/nasty/collector/Template.java
@@ -1,3 +1,35 @@
+/**************************************************************************/
+/*  NASTY - Network Analysis and STatistics Yielding                      */
+/*                                                                        */
+/*  Copyright (C) 2006 History Project, http://www.history-project.net    */
+/*  History (HIgh-Speed neTwork mOniToring and analYsis) is a research    */
+/*  project by the Universities of Tuebingen and Erlangen-Nuremberg,      */
+/*  Germany                                                               */
+/*                                                                        */
+/*  Authors of NASTY are:                                                 */
+/*      Christian Japes, University of Erlangen-Nuremberg                 */
+/*      Thomas Schurtz, University of Tuebingen                           */
+/*      David Halsband, University of Tuebingen                           */
+/*      Gerhard Muenz <muenz@informatik.uni-tuebingen.de>,                */
+/*          University of Tuebingen                                       */
+/*      Falko Dressler <dressler@informatik.uni-erlangen.de>,             */
+/*          University of Erlangen-Nuremberg                              */
+/*                                                                        */
+/*  This program is free software; you can redistribute it and/or modify  */
+/*  it under the terms of the GNU General Public License as published by  */
+/*  the Free Software Foundation; either version 2 of the License, or     */
+/*  (at your option) any later version.                                   */
+/*                                                                        */
+/*  This program is distributed in the hope that it will be useful,       */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/*  GNU General Public License for more details.                          */
+/*                                                                        */
+/*  You should have received a copy of the GNU General Public License     */
+/*  along with this program; if not, write to the Free Software           */
+/*  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  */
+/**************************************************************************/
+
 package de.japes.net.nasty.collector;
 /*
  * Created on 20.07.2004
